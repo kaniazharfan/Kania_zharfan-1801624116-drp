448bf3d636c66fd13b5f3b1279c2323bfffce4f5
“alicazahia” <alica.zahiairdi13@gmail>
2026-06-05 22:29:57 +0700

    emotion_preview.py

---
+++ Tugas 09 - Kelompok 03/activity_preview.py
@@ -0,0 +1,19 @@
+def tampilkan_activity_logic():
+
+    print("""
+========================================
+          TO-DO ACTIVITY
+========================================
+
+Input Aktivitas
+      ↓
+Simpan Aktivitas
+      ↓
+Tampilkan Daftar Aktivitas
+      ↓
+Ringkasan Aktivitas
+      ↓
+Kembali ke Menu Fitur
+""")
+
+    input("Tekan Enter untuk kembali...")
---
+++ Tugas 09 - Kelompok 03/emotion_preview.py
@@ -0,0 +1,19 @@
+def tampilkan_emotional_logic():
+
+    print("""
+========================================
+        EMOTIONAL ANALYTICS
+========================================
+
+Input Emosi
+      ↓
+Analisis Emosi
+      ↓
+Menentukan Tingkat Stres
+      ↓
+Menyimpan Hasil Analisis
+      ↓
+Kembali ke Menu Fitur
+""")
+
+    input("Tekan Enter untuk kembali...")
