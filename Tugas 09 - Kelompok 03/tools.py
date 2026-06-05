acfa69134fb91b154626d390b66007c5a2437487
Sabrina Zarlis <szarlis@gmail.com>
2026-06-05 22:11:36 +0700

    Menambahkan tools.py

---
+++ Tugas 09 - Kelompok 03/tools.py
@@ -0,0 +1,9 @@
+def tampilkan_tools(user):
+
+    print(f"\nHalo, {user}!")
+    print("\n===== MENU UTAMA =====")
+    print("1. Fitur")
+    print("2. Tentang Aplikasi")
+    print("3. Keluar")
+
+    return input("Pilih menu: ")
