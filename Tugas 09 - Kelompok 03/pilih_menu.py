e572b841aa105151d04d0a39cf5e3f72993891a5
Kania Zharfan <kaniazharfan0508@gmail.com>
2026-06-05 22:16:52 +0700

    menambahkan pilih_menu.py

---
+++ Tugas 09 - Kelompok 03/pilih_menu.py
@@ -0,0 +1,9 @@
+def pilih_menu():
+
+    print("\n===== MENU FITUR =====")
+    print("1. Emotional Analytics")
+    print("2. To-Do Activity")
+    print("3. Focus Timer")
+    print("4. Kembali ke Menu Utama")
+
+    return input("Pilih fitur: ")
