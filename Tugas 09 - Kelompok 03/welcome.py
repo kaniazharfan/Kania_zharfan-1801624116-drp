bbd7ec0ba126fc93e8694df3fe44285ca8d830b6
Kania Zharfan <kaniazharfan0508@gmail.com>
2026-06-05 22:02:57 +0700

    menambahkan tugas 09 - kelompok 03

---
+++ Tugas 09 - Kelompok 03/welcome.py
@@ -0,0 +1,9 @@
+def tampilkan_welcome():
+    print("""
+========================================
+ STUDENT EMOTION ANALYTIC TOOL (SEAT)
+========================================
+""")
+
+    nama = input("Masukkan nama: ")
+    return nama
